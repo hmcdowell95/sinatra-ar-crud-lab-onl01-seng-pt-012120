eae1d2157aa0fa13212a30bb513ba122dec05f7f
Harley McDowell <hmcdowell95@gmail.com>
2020-04-28 20:30:14 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 
 require_relative '../../config/environment'
-
+require 'pry'
 class ApplicationController < Sinatra::Base
 
   configure do
@@ -9,5 +9,44 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/' do
+    "Welcome to my Article database"
   end
+  
+  get '/articles/new' do
+    erb :new 
+  end
+  
+  post '/articles' do 
+    @article = Article.create(title: params[:title], content: params[:content])
+    
+    @id = @article.id
+    redirect "/articles/#{@id}"
+  end
+  
+  get '/articles' do 
+    @articles = Article.all
+    erb :index
+  end
+  
+  get '/articles/:id' do
+    @article = Article.find(params[:id])
+    erb :show
+  end
+  
+  get '/articles/:id/edit' do 
+    @article = Article.find(params[:id])
+    erb :edit
+  end
+  
+  patch '/articles/:id' do
+    @article = Article.find(params[:id])
+    @article.update(title: params[:title], content: params[:content])
+    erb :show
+  end
+  
+  delete '/articles/:id/delete' do
+    Article.destroy(params[:id])
+    erb :index
+  end
+    
 end
